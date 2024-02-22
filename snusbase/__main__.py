799d61c3eeb1691f272b4658ae703b012cc45913
igna <148277185+ignaigna@users.noreply.github.com>
2024-02-22 07:13:23 +0000

    fix wildcard

--- snusbase/__main__.py
+++ snusbase/__main__.py
@@ -42,19 +42,20 @@ class SnusbaseClient:
         request = await self.session.request(method, url, params=params, json=data)
         return request.json()
 
-    async def search(self, term: str, search_type: str) -> dict:
+    async def search(self, term: str, search_type: str, wildcard: bool = False) -> dict:
         """
         Search Snusbase for data related to a term.
 
         Args:
             term (str): The term to search for.
             search_type (str): The type of search to perform.
+            wildcard (bool, optional): Whether to use wildcard search.
 
         Returns:
             dict: The response data containing the search results.
         """
         endpoint = "/data/search"
-        data = {"terms": [term], "types": [search_type]}
+        data = {"terms": [term], "types": [search_type], "wildcard": wildcard}
         return await self.request("POST", endpoint, data=data)
 
     async def hash_lookup(self, _hash: str) -> dict:
@@ -161,15 +162,3 @@ class SnusbaseClient:
             dict: The response data containing the search results.
         """
         return await self.search(_hash, "hash")
-
-    async def search_by_wildcard(self, wildcard: str) -> dict:
-        """
-        Search Snusbase for data related to a wildcard.
-
-        Args:
-            wildcard (str): The wildcard to search for.
-
-        Returns:
-            dict: The response data containing the search results.
-        """
-        return await self.search(wildcard, "wildcard")
